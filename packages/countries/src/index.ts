82e73e306dff21c7bcb51db848598466daaccb9c
Hassan Khan <contact@hassankhan.me>
2019-05-20 13:15:03 +0100

    feat(countries): refactor functions

--- packages/countries/src/index.ts
+++ packages/countries/src/index.ts
@@ -2,12 +2,21 @@ import Countries from 'world-countries';
 
 import * as Types from './types';
 
-export type Country = any;
+export type Country = Types.Country;
 
 export const countries: Types.Country[] = Countries;
 
-export const findCountryByName = (name: string): Country[] => {
+export const findCountryByName = (name: string): Country | undefined => {
+  const [firstResult] = findCountriesByName(name);
+  return firstResult;
+};
+
+export const findCountriesByName = (name: string): Country[] => {
   return countries.filter((country) => {
-    [country.name, ...country.altSpellings].includes(name);
+    return [
+      country.name.official,
+      country.name.common,
+      ...country.altSpellings,
+    ].includes(name.trim());
   });
 };
