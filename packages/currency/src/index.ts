d6add49d5313e419ad1d6015976bd9b53f0a2e9f
Hassan Khan <contact@hassankhan.me>
2019-05-28 18:54:14 +0100

    docs(currency): add docblocks to Currency utility functions

--- packages/currency/src/index.ts
+++ packages/currency/src/index.ts
@@ -1,6 +1,12 @@
 import accounting from 'accounting';
 import currencyFormatter, { Currency } from 'currency-formatter';
 
+/**
+ * Converts a string containing a price value with symbol to an integer
+ * @param {string} currencyCode
+ * @param {string} price
+ * @returns {number}
+ */
 export const getAmountFromPriceString = (
   currencyCode: string,
   price: string,
@@ -14,12 +20,29 @@ export const getAmountFromPriceString = (
   return parseInt(formattedValue, 10);
 };
 
+/**
+ * Retrieves currency information for a given `currencyCode`
+ * @type {(currencyCode: string) => Currency}
+ */
 export const getCurrencyInformation = currencyFormatter.findCurrency;
 
+/**
+ * Formats a number into a string containing a price value with symbol
+ * @type {(value: number, options: {code?: string; locale?: string; symbol?: string; decimal?: string; thousand?: string; precision?: number; format?: string | {pos: string; neg: string; zero: string}}) => string}
+ */
 export const format = currencyFormatter.format;
 
+/**
+ * Formats a string containing a price value with symbol into a number. Does
+ * the opposite of `Currency#format()`
+ * @type {(value: number, options: {code?: string; locale?: string; symbol?: string; decimal?: string; thousand?: string; precision?: number; format?: string | {pos: string; neg: string; zero: string}}) => string}
+ */
 export const unformat = currencyFormatter.unformat;
 
 export const currencies: Currency[] = currencyFormatter.currencies;
 
+/**
+ * Rounds a given `number` to the required `precision`.
+ * @type {(number: number, precision?: number) => string}
+ */
 export const toFixed = accounting.toFixed;
